899941effc64dba2b673cb63522265801f81e16d
Your Name <you@example.com>
2026-07-17 11:23:54 +0400

    feat(knowledge-fabric): complete KF-1 knowledge foundation

---
+++ apps/api/src/knowledge-fabric/kf1-foundation/index.ts
@@ -0,0 +1,5 @@
+export * from "./knowledge-foundation.module";
+export * from "./knowledge-foundation.service";
+export * from "./knowledge-foundation.repository";
+export * from "./knowledge-registry.service";
+export * from "./knowledge-foundation.types";
